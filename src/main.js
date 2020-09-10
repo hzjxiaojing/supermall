2debd56179551026e0923213d2778cba98f47f35
huangzj <1359445635@qq.com>
2020-09-10 15:36:38 +0800

    '首页'

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+Vue.config.productionTip = false
 
-createApp(App).mount('#app')
+new Vue({
+  render: h => h(App),
+  router
+}).$mount('#app')
